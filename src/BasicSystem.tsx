b864fa89663e11a16ab6ec22e3bbcad20708a4d2
HexaField <joshfield999@gmail.com>
2024-03-05 14:18:50 +1100

    EntityUUID

--- src/BasicSystem.tsx
+++ src/BasicSystem.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 
-import { EntityUUID } from '@etherealengine/common/src/interfaces/EntityUUID'
+import { EntityUUID } from '@etherealengine/ecs'
 import { isClient } from '@etherealengine/common/src/utils/getEnvironment'
 import { SimulationSystemGroup, defineSystem, getComponent, setComponent } from '@etherealengine/ecs'
 import { ECSState } from '@etherealengine/ecs/src/ECSState'
